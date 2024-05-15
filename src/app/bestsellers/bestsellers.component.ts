5a07738a03b43a82aed317159c267638ab5a6f69
Juliusz Szymajda <234769@edu.p.lodz.pl>
2024-05-15 18:52:26 +0200

    All products list added to bestsellers

--- src/app/bestsellers/bestsellers.component.ts
+++ src/app/bestsellers/bestsellers.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from "../../types";
+import {WarehouseService} from "../services/warehouse.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-bestsellers',
   templateUrl: './bestsellers.component.html',
   styleUrl: './bestsellers.component.css'
 })
-export class BestsellersComponent {
+export class BestsellersComponent implements OnInit{
+  products: Product[] = [];
+  searchTextProducts: string = '';
+  filteredProducts: Product[] = [];
 
+
+  constructor(
+    private warehouseService: WarehouseService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.loadProducts();
+    });
+  }
+
+  loadProducts() {
+    this.warehouseService.getAllProducts('http://localhost:5001/products')
+      .subscribe((response: { value: Product[] }) => {
+        this.products = this.groupProductsByPrefix(response.value);
+        this.filteredProducts = this.products;
+      });
+  }
+
+  filterProducts(): void {
+    this.filteredProducts = this.products.filter(product =>
+      product.code.toLowerCase().includes(this.searchTextProducts.toLowerCase())
+    );
+  }
+
+  groupProductsByPrefix(products: Product[]): Product[] {
+    const groupedProducts: { [key: string]: Product } = {};
+    products.forEach(product => {
+      const parts = product.code.split('/');
+      const prefix = parts.slice(0, -1).join('/'); // Extract prefix without size
+      if (!groupedProducts[prefix]) {
+        // If prefix not found, add it
+        groupedProducts[prefix] = {
+          ...product,
+          code: prefix // Update code to remove size information
+        };
+      }
+    });
+    return Object.values(groupedProducts);
+  }
 }
